abf129a7b1f963593cb31cb577181e6f87f20d4b
RESHAM\user <safal1mahat@gmail.com>
2021-09-04 11:15:41 +0545

    added

--- models/index.js
+++ models/index.js
@@ -1,37 +1,30 @@
-'use strict';
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+//create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
-module.exports = db;
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+  
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+  
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = {User, Post, Comment};
